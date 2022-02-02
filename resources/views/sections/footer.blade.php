4df775265e5ce6b5416f9b650dd3ad5648de50e2
DmitriyNovok <d.novokreshennyh@key-g.com>
2022-02-03 01:44:46 +0300

    Pages about and faq

---
+++ resources/views/sections/footer.blade.php
@@ -0,0 +1,34 @@
+<!-- footer Start -->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="footer-manu">
+                    <ul>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">How it works</a></li>
+                        <li><a href="#">Support</a></li>
+                        <li><a href="#">Terms</a></li>
+                    </ul>
+                </div>
+                <p class="copyright">Copyright <?= date('Y') ?> &copy; Design & Developed. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="/plugins/jQuery/jquery.min.js"></script>
+<script src="/plugins/bootstrap/bootstrap.min.js"></script>
+<script src="/plugins/slick/slick.min.js"></script>
+<script src="/plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="/plugins/shuffle/shuffle.min.js"></script>
+<script src="/plugins/SyoTimer/jquery.syotimer.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.32/jquery.form.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.11.1/jquery.validate.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU&libraries=places"></script>
+<script src="/plugins/google-map/map.js"></script>
+<script src="/js/script.js"></script>
+
+</body>
+</html>

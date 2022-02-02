c0d94f384dd1045dea95d5dd90112880d876691e
DmitriyNovok <d.novokreshennyh@key-g.com>
2022-02-03 01:30:10 +0300

    Project files

---
+++ resources/views/sections/header.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="description" content="Aviato E-Commerce Template">
+
+    <meta name="author" content="Themefisher.com">
+
+    <title>Airspace | Creative Agency Bootstrap template</title>
+
+    <!-- Mobile Specific Meta-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.png" />
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="/css/plugins/bootstrap/bootstrap.min.css">
+    <!-- Ionic Icon Css -->
+    <link rel="stylesheet" href="/css/plugins/Ionicons/css/ionicons.min.css">
+    <!-- animate.css -->
+    <link rel="stylesheet" href="/css/plugins/animate-css/animate.css">
+    <!-- Magnify Popup -->
+    <link rel="stylesheet" href="/css/plugins/magnific-popup/magnific-popup.css">
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="/css/plugins/slick/slick.css">
+
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="/css/style.css">
+
+</head>
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/test', function () {
-    return view('test');
-});
-
-Route::get('/home', function () {
     return view('home');
 });
